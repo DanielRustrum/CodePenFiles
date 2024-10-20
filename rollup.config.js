ed1e0ae918259464921d2592f9b30edaaa20a9c2
Daniel Rustrum <DanielRustrum@users.noreply.github.com>
2024-10-20 03:58:58 +0000

    config changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,23 @@
 // rollup.config.js
-const typescript = require('@rollup/plugin-typescript')
+const typescript = require("@rollup/plugin-typescript");
+const sass = require("rollup-plugin-sass");
 
 module.exports = {
-  input: 'ts/main.ts',
+  input: "ts/main.ts",
   output: {
     sourcemap: true,
-    dir: 'public',
-    format: 'cjs'
+    dir: "public",
+    format: "cjs",
   },
-  plugins: [typescript({ 
-    sourceMap: true,
-    compilerOptions: {
-    lib: ["es5", "es6", "dom"], 
-    target: "es2022",
-    verbatimModuleSyntax: false
-  }})]
+  plugins: [
+    typescript({
+      sourceMap: true,
+      compilerOptions: {
+        lib: ["es5", "es6", "dom"],
+        target: "es2022",
+        verbatimModuleSyntax: false,
+      },
+    }),
+    sass()
+  ],
 };
