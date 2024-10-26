c8d9a97ec2c348fbd314f7807b389dd612afb1cb
Daniel Rustrum <DanielRustrum@users.noreply.github.com>
2024-10-26 05:18:20 +0000

    useScript

--- import.js
+++ import.js
@@ -1,3 +1,13 @@
 console.log("Testings")
 
-import "images";
+{
+    var CPCore = {}
+
+    CPCore.use = (module_name) => {
+        const ModuleLoader = document.querySelector("script[data-module-loader]")
+        const ScriptElement = document.createElement("script")
+        ScriptElement.setAttribute("src", `https://daniel.rustrum.net/CodePenFiles/modules/${module_name}.js`)
+
+        ModuleLoader.after(ScriptElement)
+    }
+}
