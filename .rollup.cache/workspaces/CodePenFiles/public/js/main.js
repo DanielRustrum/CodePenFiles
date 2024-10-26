95366793b07986f93b36bdd64021977168d810e0
Daniel Rustrum <DanielRustrum@users.noreply.github.com>
2024-10-26 04:57:34 +0000

    changes

--- .rollup.cache/workspaces/CodePenFiles/public/js/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = void 0;
-const log = () => console.log("tested");
-exports.log = log;
-//# sourceMappingURL=main.js.map
--- public/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var log = () => console.log("tested");
-
-exports.log = log;
-//# sourceMappingURL=main.js.map
