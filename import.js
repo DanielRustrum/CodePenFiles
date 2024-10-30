1adc8322f384e61304d56b387dc681c5e0d05cba
DanielRustrum <danieltrustrum@gmail.com>
2024-10-30 12:51:12 -0700

    dont use prototype

--- import.js
+++ import.js
@@ -1,5 +1,6 @@
 let CPEC = {}
-CPEC.prototype.modules = {}
+CPEC._hidden = {}
+CPEC._hidden.modules = {}
 console.log(CPEC)
 
 CPEC.import = async (module_name) => {
@@ -8,13 +9,13 @@ CPEC.import = async (module_name) => {
         const ScriptElement = document.createElement("script")
         ScriptElement.setAttribute("src", `https://daniel.rustrum.net/CodePenFiles/modules/${module_name}.js`)
         ScriptElement.addEventListener("load", () => {
-            resolve(CPEC.prototype.modules[module_name])
+            resolve(CPEC._hidden.modules[module_name])
         })
     
         ModuleLoader.after(ScriptElement)
     })
 }
 
-CPEC.prototype.export = async (module_name, api) => {
-    CPEC.prototype.modules[module_name] = api
+CPEC._hidden.export = async (module_name, api) => {
+    CPEC._hidden.modules[module_name] = api
 }
