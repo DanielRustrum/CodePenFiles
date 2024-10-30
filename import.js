36a990594c85a20df2b4052593259198d09b8d03
DanielRustrum <danieltrustrum@gmail.com>
2024-10-30 12:32:40 -0700

    tryHead

--- import.js
+++ import.js
@@ -1,12 +1,12 @@
 async function importScript(module_name) {
     return new Promise((resolve, reject) => {     
-        const ModuleLoader = document.querySelector("script[data-module-loader]")
+        const ModuleLoader = document.querySelector("head")
         const ScriptElement = document.createElement("script")
         ScriptElement.setAttribute("src", `https://daniel.rustrum.net/CodePenFiles/modules/${module_name}.js`)
         ScriptElement.addEventListener("load", () => {
             resolve()
         })
     
-        ModuleLoader.after(ScriptElement)
+        ModuleLoader.appendChild(ScriptElement)
     })
 }
