3fdbdf1b8b782c9cc09c2f77060a77ec9e657d90
DanielRustrum <danieltrustrum@gmail.com>
2024-10-30 12:05:52 -0700

    async import?

--- import.js
+++ import.js
@@ -1,7 +1,12 @@
-function importScript(module_name) {
-    const ModuleLoader = document.querySelector("script[data-module-loader]")
-    const ScriptElement = document.createElement("script")
-    ScriptElement.setAttribute("src", `https://daniel.rustrum.net/CodePenFiles/modules/${module_name}.js`)
-
-    ModuleLoader.after(ScriptElement)
+async function importScript(module_name) {
+    return new Promise((resolve, reject) => {     
+        const ModuleLoader = document.querySelector("script[data-module-loader]")
+        const ScriptElement = document.createElement("script")
+        ScriptElement.setAttribute("src", `https://daniel.rustrum.net/CodePenFiles/modules/${module_name}.js`)
+        ScriptElement.addEventListener("load", () => {
+            resolve()
+        })
+    
+        ModuleLoader.after(ScriptElement)
+    })
 }
