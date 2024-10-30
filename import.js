8e348e72d0e946cc4d544166a8ac16eb0bb328e5
DanielRustrum <danieltrustrum@gmail.com>
2024-10-30 12:42:26 -0700

    back to body

--- import.js
+++ import.js
@@ -3,14 +3,14 @@ CPEC.prototype.modules = {}
 
 CPEC.import = async (module_name) => {
     return new Promise((resolve, reject) => {     
-        const ModuleLoader = document.querySelector("head")
+        const ModuleLoader = document.querySelector("script[data-module-loader]")
         const ScriptElement = document.createElement("script")
         ScriptElement.setAttribute("src", `https://daniel.rustrum.net/CodePenFiles/modules/${module_name}.js`)
         ScriptElement.addEventListener("load", () => {
             resolve(CPEC.prototype.modules[module_name])
         })
     
-        ModuleLoader.appendChild(ScriptElement)
+        ModuleLoader.after(ScriptElement)
     })
 }
 
