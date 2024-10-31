ff7232c0c1f7ecc7e5444746a3682cf53335feb8
DanielRustrum <danieltrustrum@gmail.com>
2024-10-30 18:48:25 -0700

    add dev mode

--- import.js
+++ import.js
@@ -2,11 +2,15 @@ let CPEC = {}
 CPEC._hidden = {}
 CPEC._hidden.modules = {}
 
-CPEC.import = async (module_name) => {
+CPEC.import = async (module_name, dev=false) => {
     return new Promise((resolve, reject) => {     
         const ModuleLoader = document.querySelector("script[data-module-loader]")
         const ScriptElement = document.createElement("script")
-        ScriptElement.setAttribute("src", `https://daniel.rustrum.net/CodePenFiles/modules/${module_name}.js`)
+
+        let module_url = "https://daniel.rustrum.net/CodePenFiles/modules"
+        if(dev) module_url = "./modules";
+
+        ScriptElement.setAttribute("src", `${module_url}/${module_name}.js`)
         ScriptElement.addEventListener("load", () => {
             resolve(CPEC._hidden.modules[module_name])
         })
