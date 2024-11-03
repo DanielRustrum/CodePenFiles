a47bb984c608a8c92ef5f7dc7276c5234e1b1d8a
DanielRustrum <danieltrustrum@gmail.com>
2024-11-02 18:03:52 -0700

    fixed import caching?

--- import.js
+++ import.js
@@ -1,16 +1,24 @@
 let CPEC = {}
 CPEC._hidden = {}
 CPEC._hidden.modules = {}
+CPEC._hidden.m_map = {}
 
 CPEC.import = async (module_name, dev=false) => {
     return new Promise((resolve, reject) => {     
+        if(Object.keys(CPEC._hidden.modules).includes(module_name)) 
+            resolve(CPEC._hidden.modules[module_name]);
+
         const ModuleLoader = document.querySelector("script[data-module-loader]")
         const ScriptElement = document.createElement("script")
 
         let module_url = "https://daniel.rustrum.net/CodePenFiles/modules"
         if(dev) module_url = "./modules";
 
-        ScriptElement.setAttribute("src", `${module_url}/${module_name}.js`)
+        const uuid = crypto.randomUUID();
+        const script_url = `${module_url}/${module_name}.js?v=${uuid}`
+        CPEC._hidden.m_map[module_name] = script_url
+
+        ScriptElement.setAttribute("src", script_url)
         ScriptElement.addEventListener("load", () => {
             resolve(CPEC._hidden.modules[module_name])
         })
